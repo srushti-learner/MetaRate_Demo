6fd4ddac9bc7e03a4d5b8bb7007bde068aa64e82
Srushti Giridhar Borate <borate_sg@ee.iitr.ac.in>
2024-04-19 00:31:55 +0530

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-from database import Base
-from sqlalchemy import Column,Integer,String
-
-class User(Base):
-    __tablename__ = 'user'
-    id = Column(Integer, primary_key=True,index=True)
-    name=Column(String(100),nullable=False)
-    answer = Column(String(50),nullable=False)
